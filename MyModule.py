2e92e15ae1c0a246857d79e733f66eb88804c367
Shamsheera  A T <96409324+shamsheerafarsheed@users.noreply.github.com>
2025-07-30 19:50:39 +0530

    Add files via upload

---
+++ MyModule.py
@@ -0,0 +1,6 @@
+def greet(name):
+    return f"Welcome, {name}! You have successfully logged in."
+def calculate_salary(basic, bonus):
+    return basic + bonus
+
+company_name = "Tech Solutions Pvt Ltd"
---
+++ mainCall_CustomModule.py
@@ -0,0 +1,2 @@
+import MyModule
+print(MyModule.greet("John"))    # Using a function from custom moduleprint("Total Salary:", mymodule.calculate_salary(40000, 5000))print("Company:", mymodule.company_name)
